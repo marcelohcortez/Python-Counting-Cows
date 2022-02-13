42cb373b46d08099f926f188eca9d2c03d3d7b56
Marcelo Henriques Cortez <marcelohcortez@gmail.com>
2022-02-13 09:12:04 +0000

    Add the class and comments

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-class Counting_Cows:
-   
-    def cows_number(self, year: int) -> int:
+""""
+Count the number of cows after 'n' years. We start with 1 cow, and we know that: 
+- Each cow gives birth to one calf every year;
+- A calf grows into a cow after 2 years;
+- Cows and calves never die.
+"""
+class Counting_Cows: #create the class that will handle the functions
+    def cows_number(self, year: int) -> int: #count the cows after n years
       if year < 0:
         return -1
       elif year >= 0 and year <= 2:
@@ -8,14 +13,14 @@ class Counting_Cows:
       else:
         return year -1
 
-    def get_total(self, year: int) -> int:
+    def get_total(self, year: int) -> int:#get the total based on the input year
       total = 0
       i = 0
-      for i in range(year):
+      for i in range(year):#iterate once for every year on the input
         total += self.cows_number(i)
 
       print(total)
 
-count = Counting_Cows()
+count = Counting_Cows() #initialize the class and set it to the variable count
 
-count.get_total(5)
+count.get_total(15) #use the get_total function inside the class to pass the number of years
